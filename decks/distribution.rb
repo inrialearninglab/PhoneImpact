5ab3add40956465f0a14e2c6b66d6cd3beb2cb95
lfarhi <laurence.farhi@inria.fr>
2023-07-17 17:47:15 +0200

    ajout metal précieux dans la desc de la carte mere

--- decks/distribution.rb
+++ decks/distribution.rb
@@ -2,7 +2,7 @@
 # Ecran tactile: 2 Plastique, 1 Métal rare, 1 Terre rare, 1 Métal commun
 # Batterie: 1 Plastique, 2 Métal rare, 1 Métal commun
 # Coque: 3 Plastique, 1 Métal rare, 1 Métal commun
-# Carte Mère: 2 Plastique, 2 Métal rare, 2 Métal commun, 1 Terre rare
+# Carte Mère: 2 Plastique, 2 Métal rare, 2 Métal commun, 1 Terre rare, 1 Métal précieux
 
 
 # -------------------- Répartition des pioches --------------------
@@ -46,35 +46,35 @@ def defineDecks(nbPlayers)
     cards['recyclage']['metalCommun'] = (metalCommun * nbPlayers * 0.2).round
     # Peut être faire un surplus pour les métaux précieux ici
     cards['recyclage']['metalPrecieux'] = (metalPrecieux * nbPlayers * 0.4).round
-    
+
     # Restant:
     # Plastique: 80% des cartes totales
     # Métal commun: 80% des cartes totales
     # Métal rare: 100% des cartes totales
     # Terre rare: 100% des cartes totales
     # Métal précieux: 80% des cartes totales
-    
+
     # Polluant moins: 40% des cartes totales, contient: Plastiques, Métal commun, Métal rare, Terres rares et Métaux précieux
     cards['polluant_moins']['plastique'] = (plastique * nbPlayers * 0.4).round
     cards['polluant_moins']['metalCommun'] = (metalCommun * nbPlayers * 0.4).round
     cards['polluant_moins']['metalRare'] = (metalRare * nbPlayers * 0.5).round
     cards['polluant_moins']['terreRare'] = (terreRare * nbPlayers * 0.4).round
     cards['polluant_moins']['metalPrecieux'] = (metalPrecieux * nbPlayers * 0.3).round
-    
+
     # Restant:
     # Plastique: 40% des cartes totales
     # Métal commun: 40% des cartes totales
     # Métal rare: 50% des cartes totales
     # Terre rare: 60% des cartes totales
     # Métal précieux: 40% des cartes totales
-    
+
     # Polluant plus: 40% des cartes totales, contient: Plastiques, Métal commun, Métal rare, Terres rares et Métaux précieux
     cards['polluant_plus']['plastique'] = (plastique * nbPlayers * 0.4).round
     cards['polluant_plus']['metalCommun'] = (metalCommun * nbPlayers * 0.4).round
     cards['polluant_plus']['metalRare'] = (metalRare * nbPlayers * 0.5).round
     cards['polluant_plus']['terreRare'] = (terreRare * nbPlayers * 0.6).round
     cards['polluant_plus']['metalPrecieux'] = (metalPrecieux * nbPlayers * 0.3).round
-    
+
     return cards
 end
 
