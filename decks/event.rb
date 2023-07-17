c4cdcc9048f096edf0b58bfaed2df3aaadce52ab
lfarhi <laurence.farhi@inria.fr>
2023-07-17 16:57:26 +0200

    modif typage des evts

--- decks/event.rb
+++ decks/event.rb
@@ -3,38 +3,38 @@ require_relative 'common'
 layouts = ['economy.yml', 'templates/event.yml']
 color = '#0a4969'
 events = [
-    'Chaque joueur prend une carte à son voisin de droite commencant par celui qui a le moins de malus, sinon le plus de carte recyclées',
-    'Chaque joueur prend une carte à ses voisins ayant plus de malus que lui',
-    'Chaque joueur pioche une carte de son choix parmi celles visibles dans l\'ordre croissant de malus (recyclage si égalité)',
-    'Chaque joueur prend une carte à son voisin de droite commencant par celui qui a le moins de malus, sinon le plus de carte recyclées',
-    'Chaque joueur perd les ressources pour en avoir 2 maximums de chaque type en excluant les ressources recyclées',
-    'Chaque joueur perd autant de cartes que de la moitié du nombre de ses malus (arrondi à l\'inférieur)',
-    'Chaque joueur prend une carte à ses voisins ayant strictement plus de malus que lui',
-    'Les malus des pioches sont doublés jusqu\'à la fin de la partie',
-    'Chaque joueur pioche une carte de son choix parmi celles visibles dans l\'ordre croissant de malus',
-    'Chaque joueur perd autant de cartes que de la moitié du nombre de ses malus',
-    'Tous les joueurs ayant au moins 5 cartes recyclées (main + téléphone) piochent 2 cartes',
-    'Chaque joueur prend une carte à ses voisins ayant plus de malus que lui',
-    'Chaque joueur perd les ressources pour en avoir 2 maximums de chaque type en excluant les ressources recyclées',
-    'Tous les joueurs ayant moins de 3 cartes recyclées défaussent 2 cartes',
-    'FIN DE PARTIE'
+    'Chaque joueur prend une carte à son voisin de droite commencant par celui qui a le moins de malus, sinon le plus de cartes recyclées', #1
+    'Chaque joueur prend une carte à ses voisins ayant plus de malus que lui', #2
+    'Chaque joueur pioche une carte de son choix parmi celles visibles dans l\'ordre croissant de malus (recyclage si égalité)', #3
+    'Chaque joueur perd les ressources pour en avoir 2 maximums de chaque type en excluant les ressources recyclées', #4
+    'Chaque joueur perd autant de cartes que de la moitié du nombre de ses malus (arrondi à l\'inférieur)', #5
+    'Chaque joueur prend une carte à ses voisins ayant strictement plus de malus que lui', #6
+    'Les malus des pioches sont doublés jusqu\'à la fin de la partie', #7
+    'Chaque joueur pioche une carte de son choix parmi celles visibles dans l\'ordre croissant de malus', #8
+    'Chaque joueur perd autant de cartes que de la moitié du nombre de ses malus', #9
+    'Tous les joueurs ayant au moins 5 cartes recyclées (main + téléphone) piochent 2 cartes', #10
+    'Chaque joueur prend une carte à son voisin de droite commencant par celui qui a le moins de malus, sinon le plus de cartes recyclées', #11
+    'Chaque joueur prend une carte à ses voisins ayant plus de malus que lui', #12
+    'Chaque joueur perd les ressources pour en avoir 2 maximums de chaque type en excluant les ressources recyclées', #13
+    'Tous les joueurs ayant moins de 3 cartes recyclées défaussent 2 cartes', #14
+    'FIN DE LA PARTIE' #15
 ]
 types = [
-    'Péripétie',
-    'Duel',
-    'Bonus',
-    'Péripétie',
-    'Taxe',
-    'Taxe',
-    'Duel',
-    'Evolution',
-    'Bonus',
-    'Taxe',
-    'Bonus',
-    'Duel',
-    'Péripétie',
-    'Taxe',
-    'Evolution'
+    'Redistribution', #1
+    'Règlementation', #2
+    'Bonus', #3
+    'Règlementation', #4
+    'Règlementation', #5
+    'Règlementation', #6
+    'Événement majeur', #7
+    'Bonus', #8
+    'Règlementation', #9
+    'Bonus', #10
+    'Redistribution', #11
+    'Règlementation', #12
+    'Règlementation', #13
+    'Règlementation', #14
+    'Événement majeur' #15
 ]
 events_number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 events_illu =  Array.new(15, 'event_illu')
@@ -71,4 +71,3 @@ Squib::Deck.new cards: events.length(), layout: layouts do
     text str: events_number, layout: 'event_number', color: color
     save_pdf file: "event.pdf", width: 1800*1.5, height: 1800*297/210*1.5
 end
-
