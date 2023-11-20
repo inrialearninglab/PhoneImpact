855ce8ae4873602469b67156f46dcf7069bc6e7c
VIAUD Nathan <nathan.viaud@inria.fr>
2023-11-20 12:14:28 +0100

    adding image to malus

--- decks/pnp/malus.rb
+++ decks/pnp/malus.rb
@@ -1,9 +1,11 @@
 require 'squib'
 
 cards = {
+  'type' => [],
   'text' => [],
   'image' => [],
   'color' => [],
+  'illu' => [],
 }
 
 index = 0
@@ -15,17 +17,21 @@ black = '#000000'
 CSV.foreach('data/csv/malus.csv', headers: true) do |row|
   if index == cards_per_sheet
     cards_per_sheet.times do
+      cards['type'] << ''
       cards['text'] << ''
       cards['image'] << 'data/images/back/malus_back.png'
       cards['color'] << black
+      cards['illu'] << 'data/images/malus_img/empty.png'
     end
 
     index = 0
   end
 
+  cards['type'] << row['type']
   cards['text'] << row['text'].gsub('\n', "\n")
   cards['image'] << 'data/images/border/malus_border.png'
   cards['color'] << black
+  cards['illu'] << 'data/images/malus_img/' + row['image'] + '.png'
 
   index += 1
 end
@@ -33,17 +39,21 @@ end
 CSV.foreach('data/csv/malus_events.csv', headers: true) do |row|
   if index == cards_per_sheet
     cards_per_sheet.times do
+      cards['type'] << ''
       cards['text'] << ''
       cards['image'] << 'data/images/back/malus_back.png'
       cards['color'] << black
+      cards['illu'] << 'data/images/malus_img/empty.png'
     end
 
     index = 0
   end
 
+  cards['type'] << ''
   cards['text'] << row['text'].gsub('\n', "\n")
   cards['image'] << 'data/images/border/malus_event_border.png'
   cards['color'] << red
+  cards['illu'] << 'data/images/malus_img/empty.png'
 
   index += 1
 end
@@ -54,11 +64,13 @@ if index > 0
   remaining_cards.times do
     cards['text'] << ''
     cards['image'] << 'data/images/back/empty.png'
+    cards['illu'] << 'data/images/malus_img/empty.png'
   end
 
   index.times do
     cards['text'] << ''
     cards['image'] << 'data/images/back/malus_back.png'
+    cards['illu'] << 'data/images/malus_img/empty.png'
   end
 end
 
@@ -67,6 +79,10 @@ Squib::Deck.new cards: cards['text'].size, layout: ['layouts/malus.yml', 'layout
 
   png file: cards['image'], layout: 'background_pnp'
 
+  text str: cards['type'], layout: 'malus_type'
   text str: cards['text'], layout: 'malus_text'
+
+  png file: cards['illu'], layout: 'malus_illu'
+
   save_pdf file: 'malus.pdf', dir: '_output/pnp', sprue: 'sprues/pnp.yml'
 end
