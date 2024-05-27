b82e8c49568155188f4fbf6b064a3b59b5fb8318
VIAUD Nathan <nathan.viaud@inria.fr>
2024-05-27 14:34:28 +0200

    improvements: event number & type on the same line

--- decks/pnp/events.rb
+++ decks/pnp/events.rb
@@ -1,7 +1,6 @@
 require 'squib'
 
 cards = {
-  'number' => [],
   'type' => [],
   'text' => [],
   'image' => [],
@@ -13,7 +12,6 @@ cards_per_sheet = 8
 CSV.foreach('data/csv/events.csv', headers: true) do |row|
   if index == cards_per_sheet
     cards_per_sheet.times do
-      cards['number'] << ''
       cards['type'] << ''
       cards['text'] << ''
       cards['image'] << 'data/images/back/event_back.png'
@@ -22,8 +20,9 @@ CSV.foreach('data/csv/events.csv', headers: true) do |row|
     index = 0
   end
 
-  cards['number'] << row['number']
-  cards['type'] << row['type']
+  type = row['number'].concat('. ').concat(row['type'])
+
+  cards['type'] << type
   cards['text'] << row['text'].gsub('\n', "\n")
   cards['image'] << 'data/images/border/event_border.png'
 
@@ -34,28 +33,25 @@ if index > 0
   remaining_cards = cards_per_sheet - index
 
   remaining_cards.times do
-    cards['number'] << ''
     cards['type'] << ''
     cards['text'] << ''
     cards['image'] << 'data/images/back/empty.png'
   end
 
   index.times do
-    cards['number'] << ''
     cards['type'] << ''
     cards['text'] << ''
     cards['image'] << 'data/images/back/event_back.png'
   end
 end
 
-Squib::Deck.new cards: cards['number'].size, layout: ['layouts/events.yml', 'layouts/common.yml'] do
+Squib::Deck.new cards: cards['type'].size, layout: ['layouts/events.yml', 'layouts/common.yml'] do
   background color: 'white'
 
   png file: cards['image'], layout: 'background_pnp'
 
-  text str: cards['number'], layout: 'event_number'
   text str: cards['type'], layout: 'event_type'
-  text str: cards['text'], layout: 'event_text'
+  text str: cards['text'], layout: 'event_text', hint: :red
 
   save_pdf file: 'events.pdf', dir: '_output/pnp', sprue: 'sprues/pnp.yml'
 end
