b79d06197e21c8b56217da00704d4af57a42394b
VIAUD Nathan <nathan.viaud@inria.fr>
2024-06-24 14:03:39 +0200

    feat: adding qrcode to last event

--- decks/pnp/events.rb
+++ decks/pnp/events.rb
@@ -5,6 +5,7 @@ cards = {
   'type' => [],
   'text' => [],
   'image' => [],
+  'illu' => [],
 }
 
 index = 0
@@ -16,16 +17,22 @@ CSV.foreach('data/csv/events.csv', headers: true) do |row|
       cards['type'] << ''
       cards['text'] << ''
       cards['image'] << 'data/images/back/event_back.png'
+      cards['illu'] << 'data/images/icons/empty.png'
     end
 
     index = 0
   end
 
-  type = row['number'].concat('. ').concat(row['type'])
+  type = row['number'] + '. ' + row['type']
 
   cards['type'] << type
   cards['text'] << row['text'].gsub('\n', "\n")
   cards['image'] << 'data/images/border/event_border.png'
+  if row['number'] == '14'
+    cards['illu'] << 'data/images/icons/QR.png'
+  else
+    cards['illu'] << 'data/images/icons/empty.png'
+  end
 
   index+= 1
 end
@@ -37,11 +44,13 @@ if index > 0
     cards['type'] << ''
     cards['text'] << ''
     cards['image'] << 'data/images/back/empty.png'
+    cards['illu'] << 'data/images/icons/empty.png'
   end
 
   (1..cards_per_sheet).each { |i|
     cards['type'] << ''
     cards['text'] << ''
+    cards['illu'] << 'data/images/icons/empty.png'
     if PosMap.map[i] > index
       cards['image'] << 'data/images/back/empty.png'
     else
@@ -57,6 +66,8 @@ Squib::Deck.new cards: cards['type'].size, layout: ['layouts/events.yml', 'layou
 
   text str: cards['type'], layout: 'event_type'
   text str: cards['text'], layout: 'event_text'
+
+  png file: cards['illu'], layout: 'event_illu'
 
   save_pdf file: 'events.pdf', dir: '_output/pnp', sprue: 'sprues/pnp.yml'
 end
